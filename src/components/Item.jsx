7e73cc506a9687bdad811df4c589e9ecedc98612
Carlos Torres <ctgovea@gmail.com>
2023-08-21 21:22:12 -0600

    refactor out DeleteIcon

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import EditableMoneyLabel from './EditableMoneyLabel';
+import DeleteIcon from './DeleteIcon';
 
-const Item = ({
-  item,
-  index,
-  editingIndex,
-  handleAmountEdit,
-  removeItem,
-  provided,
-}) => {
+const Item = ({ item, index, handleAmountEdit, removeItem, provided }) => {
   return (
     <div
       key={index}
@@ -22,32 +16,9 @@ const Item = ({
       <EditableMoneyLabel
         index={index}
         amount={item.amount}
-        handleAmountEdit={handleAmountEdit}
+        onUpdate={handleAmountEdit}
       />
-      <button
-        className={
-          'text-gray-400 ml-2 hidden ' +
-          (editingIndex === index
-            ? ''
-            : 'group-hover:block hover:text-gray-600')
-        }
-        onClick={() => removeItem(index)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-4 h-4"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-          />
-        </svg>
-      </button>
+      <DeleteIcon index={index} onClick={removeItem} />
       {/* TODO Add description */}
     </div>
   );
